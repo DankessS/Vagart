d69431d6aa175605f93e2048d65610a844813a35
Daniel <danekp0510@vp.pl>
2018-11-22 23:31:50 +0100

    Graphics og:image fix

--- src/app/components/graphics/graphics.component.ts
+++ src/app/components/graphics/graphics.component.ts
@@ -15,7 +15,7 @@ export class GraphicsComponent implements OnInit {
     this.meta.updateTag({property:'og:title', content:'Projekty graficzne,zaproszenia ślubne, dekoracje, loga, winietki oraz wiele innych - VAGart.pl'});
     this.meta.updateTag({property:'og:url', content:'https://vagart.pl/grafika'});
     this.meta.updateTag({property:'og:description', content:'Realizujemy wyszukane projekty graficzne, zaproszenia ślubne, dekoracje, loga, winietki, banery, zawieszki, wizytówki, identyfikazję wizualną firmy.'});
-    this.meta.updateTag({property:'og:image', content:'../../../assets/img/grafika/papeteria/zaproszenia/zaproszenia3.JPG'});
+    this.meta.updateTag({property:'og:image', content:'https://vagart.pl/assets/img/grafika/papeteria/zaproszenia/zaproszenia3.JPG'});
     this.meta.updateTag({property:'og:image:alt', content:'Realizujemy wyszukane projekty graficzne, zaproszenia ślubne, dekoracje, loga, winietki, banery, zawieszki, wizytówki, identyfikazję wizualną firmy.'});
     document.body.style.background = "url('../../../assets/img/GRAFIKA.png') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
