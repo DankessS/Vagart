bad4df43ecdd7921aba4dc8e920c02ed7b475e08
Daniel <danekp0510@vp.pl>
2018-12-28 19:37:35 +0100

    cards

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/banery --outputDir src/assets/img/gallery/banners --gName=banners
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/bileciki_na_kolacz --outputDir src/assets/img/gallery/cakeTickets --gName=cakeTickets
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/dekoracje --outputDir src/assets/img/gallery/decoration --gName=decoration
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/koszyczek_ratunkowy --outputDir src/assets/img/gallery/helpTicket --gName=helpTicket
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/ksiega_gosci --outputDir src/assets/img/gallery/guestBook --gName=guestBook
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/numeracja_stolikow --outputDir src/assets/img/gallery/table_numeration --gName=table_numeration
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/menu --outputDir src/assets/img/gallery/menu --gName=menu
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/plakaty --outputDir src/assets/img/gallery/poster --gName=poster
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/plan_usadzenia_gosci --outputDir src/assets/img/gallery/sitPlan --gName=sitPlan
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/tablice_rejestracyjne --outputDir src/assets/img/gallery/registrationTables --gName=registrationTables
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/toppery --outputDir src/assets/img/gallery/toppers --gName=toppers
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/winietki --outputDir src/assets/img/gallery/vignettes --gName=vignettes
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/zaproszenia --outputDir src/assets/img/gallery/invitations --gName=invitations
+node node_modules/angular2-image-gallery/convert.js src/assets/img/grafika/papeteria/zawieszki --outputDir src/assets/img/gallery/pendants --gName=pendants
