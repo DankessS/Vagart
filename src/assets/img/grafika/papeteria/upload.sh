06724f7311e382f6474ec466e89c0e725c09b817
Daniel <danekp0510@vp.pl>
2019-01-15 21:29:09 +0100

    Sort type last date, fixes

--- src/assets/img/grafika/papeteria/upload.sh
+++ src/assets/img/grafika/papeteria/upload.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/banery --outputDir /var/www/vagart/code/src/assets/img/gallery/banners --gName=banners
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/banery --outputDir /var/www/vagart/code/src/assets/img/gallery/banners --gName=banners -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/banners /var/www/vagart/code/dist/browser/assets/img/gallery/banners
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/bileciki_na_kolacz --outputDir /var/www/vagart/code/src/assets/img/gallery/cakeTickets --gName=cakeTickets
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/bileciki_na_kolacz --outputDir /var/www/vagart/code/src/assets/img/gallery/cakeTickets --gName=cakeTickets -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/cakeTickets /var/www/vagart/code/dist/browser/assets/img/gallery/cakeTickets
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/dekoracje --outputDir /var/www/vagart/code/src/assets/img/gallery/decoration --gName=decoration
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/dekoracje --outputDir /var/www/vagart/code/src/assets/img/gallery/decoration --gName=decoration -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/decoration /var/www/vagart/code/dist/browser/assets/img/gallery/decoration
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/koszyczek_ratunkowy --outputDir /var/www/vagart/code/src/assets/img/gallery/helpTicket --gName=helpTicket
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/koszyczek_ratunkowy --outputDir /var/www/vagart/code/src/assets/img/gallery/helpTicket --gName=helpTicket -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/helpTicket /var/www/vagart/code/dist/browser/assets/img/gallery/helpTicket
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/ksiega_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/guestBook --gName=guestBook
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/ksiega_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/guestBook --gName=guestBook -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/guestBook /var/www/vagart/code/dist/browser/assets/img/gallery/guestBook
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/numeracja_stolikow --outputDir /var/www/vagart/code/src/assets/img/gallery/table_numeration --gName=table_numeration
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/numeracja_stolikow --outputDir /var/www/vagart/code/src/assets/img/gallery/table_numeration --gName=table_numeration -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/table_numeration /var/www/vagart/code/dist/browser/assets/img/gallery/table_numeration
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/menu --outputDir /var/www/vagart/code/src/assets/img/gallery/menu --gName=menu
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/menu --outputDir /var/www/vagart/code/src/assets/img/gallery/menu --gName=menu -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/menu /var/www/vagart/code/dist/browser/assets/img/gallery/menu
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plakaty --outputDir /var/www/vagart/code/src/assets/img/gallery/poster --gName=poster
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plakaty --outputDir /var/www/vagart/code/src/assets/img/gallery/poster --gName=poster -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/poster /var/www/vagart/code/dist/browser/assets/img/gallery/poster
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plan_usadzenia_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/sitPlan --gName=sitPlan
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plan_usadzenia_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/sitPlan --gName=sitPlan -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/sitPlan /var/www/vagart/code/dist/browser/assets/img/gallery/sitPlan
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/tablice_rejestracyjne --outputDir /var/www/vagart/code/src/assets/img/gallery/registrationTables --gName=registrationTables
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/tablice_rejestracyjne --outputDir /var/www/vagart/code/src/assets/img/gallery/registrationTables --gName=registrationTables -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/registrationTables /var/www/vagart/code/dist/browser/assets/img/gallery/registrationTables
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/toppery --outputDir /var/www/vagart/code/src/assets/img/gallery/toppers --gName=toppers
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/toppery --outputDir /var/www/vagart/code/src/assets/img/gallery/toppers --gName=toppers -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/toppers /var/www/vagart/code/dist/browser/assets/img/gallery/toppers
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/winietki --outputDir /var/www/vagart/code/src/assets/img/gallery/vignettes --gName=vignettes
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/winietki --outputDir /var/www/vagart/code/src/assets/img/gallery/vignettes --gName=vignettes -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/vignettes /var/www/vagart/code/dist/browser/assets/img/gallery/vignettes
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zaproszenia --outputDir /var/www/vagart/code/src/assets/img/gallery/invitations --gName=invitations
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zaproszenia --outputDir /var/www/vagart/code/src/assets/img/gallery/invitations --gName=invitations -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/invitations /var/www/vagart/code/dist/browser/assets/img/gallery/invitations
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zawieszki --outputDir /var/www/vagart/code/src/assets/img/gallery/pendants --gName=pendants
+node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zawieszki --outputDir /var/www/vagart/code/src/assets/img/gallery/pendants --gName=pendants -d
 cp -r /var/www/vagart/code/src/assets/img/gallery/pendants /var/www/vagart/code/dist/browser/assets/img/gallery/pendants
 
 echo KONIEC
