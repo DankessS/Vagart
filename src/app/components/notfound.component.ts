d91bd8474fde95c0b5fcc5e0cd3f35a06b65a504
Daniel <danekp0510@vp.pl>
2019-01-20 22:13:29 +0100

    Angular 7 migration

--- src/app/components/notfound.component.ts
+++ src/app/components/notfound.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import {APP_ID, Component, Inject, OnInit, Optional, PLATFORM_ID} from "@angular/core";
+import {RESPONSE} from "@nguniversal/express-engine/tokens";
+import {isPlatformServer} from "@angular/common";
+
+interface PartialResponse {
+  status(code: number): this;
+}
+
 @Component({
   selector: 'notfound',
   template: `
@@ -6,4 +13,16 @@ import { Component } from "@angular/core";
   <h1>Strona o podanym adresie nie została znaleziona. Sprawdź poprawność adresu i spróbuj ponownie.</h1>
     </div>`,
 })
-export class NotFoundComponent {}
+export class NotFoundComponent implements OnInit {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string,
+    @Optional() @Inject(RESPONSE) private response: PartialResponse,
+  ) { }
+
+  ngOnInit() {
+    if (isPlatformServer(this.platformId) && this.response) {
+      this.response.status(404);
+    }
+  }
+}
