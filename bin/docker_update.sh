134a02bf5b09d3b8fde4cb5adcf88bd47fb4e99a
DankessS <danekp0510@vp.pl>
2019-02-03 12:40:47 +0100

    Docker fixes

--- bin/docker_update.sh
+++ bin/docker_update.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -e
-docker-compose -f ./bin/docker-compose.yml build --no-cache
-docker-compose -f ./bin/docker-compose.yml up -d --force-recreate
+docker-compose -f docker-compose.yml build --no-cache
+docker-compose -f docker-compose.yml up -d --force-recreate
 exit
