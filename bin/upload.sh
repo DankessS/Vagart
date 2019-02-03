f7423cc6500d55bbf8b38416e33baaae971df33d
Daniel <danekp0510@vp.pl>
2019-02-03 14:55:50 +0100

    Chance images directory

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/banery --outputDir /var/www/vagart/code/src/assets/img/gallery/banners --gName=banners
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/bileciki_na_kolacz --outputDir /var/www/vagart/code/src/assets/img/gallery/cakeTickets --gName=cakeTickets
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/dekoracje --outputDir /var/www/vagart/code/src/assets/img/gallery/decoration --gName=decoration
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/koszyczek_ratunkowy --outputDir /var/www/vagart/code/src/assets/img/gallery/helpTicket --gName=helpTicket
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/ksiega_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/guestBook --gName=guestBook
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/numeracja_stolikow --outputDir /var/www/vagart/code/src/assets/img/gallery/table_numeration --gName=table_numeration
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/menu --outputDir /var/www/vagart/code/src/assets/img/gallery/menu --gName=menu
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plakaty --outputDir /var/www/vagart/code/src/assets/img/gallery/poster --gName=poster
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/plan_usadzenia_gosci --outputDir /var/www/vagart/code/src/assets/img/gallery/sitPlan --gName=sitPlan
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/tablice_rejestracyjne --outputDir /var/www/vagart/code/src/assets/img/gallery/registrationTables --gName=registrationTables
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/toppery --outputDir /var/www/vagart/code/src/assets/img/gallery/toppers --gName=toppers
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/winietki --outputDir /var/www/vagart/code/src/assets/img/gallery/vignettes --gName=vignettes
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zaproszenia --outputDir /var/www/vagart/code/src/assets/img/gallery/invitations --gName=invitations
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/grafika/papeteria/zawieszki --outputDir /var/www/vagart/code/src/assets/img/gallery/pendants --gName=pendants
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/banery --outputDir /usr/src/app/src/assets/img/gallery/banners --gName=banners
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/bileciki_na_kolacz --outputDir /usr/src/app/src/assets/img/gallery/cakeTickets --gName=cakeTickets
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/dekoracje --outputDir /usr/src/app/src/assets/img/gallery/decoration --gName=decoration
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/koszyczek_ratunkowy --outputDir /usr/src/app/src/assets/img/gallery/helpTicket --gName=helpTicket
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/ksiega_gosci --outputDir /usr/src/app/src/assets/img/gallery/guestBook --gName=guestBook
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/numeracja_stolikow --outputDir /usr/src/app/src/assets/img/gallery/table_numeration --gName=table_numeration
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/menu --outputDir /usr/src/app/src/assets/img/gallery/menu --gName=menu
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/plakaty --outputDir /usr/src/app/src/assets/img/gallery/poster --gName=poster
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/plan_usadzenia_gosci --outputDir /usr/src/app/src/assets/img/gallery/sitPlan --gName=sitPlan
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/tablice_rejestracyjne --outputDir /usr/src/app/src/assets/img/gallery/registrationTables --gName=registrationTables
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/toppery --outputDir /usr/src/app/src/assets/img/gallery/toppers --gName=toppers
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/winietki --outputDir /usr/src/app/src/assets/img/gallery/vignettes --gName=vignettes
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/zaproszenia --outputDir /usr/src/app/src/assets/img/gallery/invitations --gName=invitations
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/grafika/papeteria/zawieszki --outputDir /usr/src/app/src/assets/img/gallery/pendants --gName=pendants
 
 
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/architektura/wnetrza/glubczyce --outputDir /var/www/vagart/code/src/assets/img/gallery/glubczyce --gName=glubczyce
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/architektura/wnetrza/myslowice --outputDir /var/www/vagart/code/src/assets/img/gallery/myslowice --gName=myslowice
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/architektura/wnetrza/tychy --outputDir /var/www/vagart/code/src/assets/img/gallery/tychy --gName=tychy
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/architektura/wnetrza/ustron --outputDir /var/www/vagart/code/src/assets/img/gallery/ustron --gName=ustron
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/architektura/wnetrza/glubczyce --outputDir /usr/src/app/src/assets/img/gallery/glubczyce --gName=glubczyce
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/architektura/wnetrza/myslowice --outputDir /usr/src/app/src/assets/img/gallery/myslowice --gName=myslowice
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/architektura/wnetrza/tychy --outputDir /usr/src/app/src/assets/img/gallery/tychy --gName=tychy
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/architektura/wnetrza/ustron --outputDir /usr/src/app/src/assets/img/gallery/ustron --gName=ustron
 
-node /var/www/vagart/code/node_modules/angular2-image-gallery/convert.js /var/www/vagart/code/src/assets/img/architektura/meble --outputDir /var/www/vagart/code/src/assets/img/gallery/furnitures --gName=furnitures
+node /usr/src/app/node_modules/angular2-image-gallery/convert.js /usr/src/app/src/assets/img/architektura/meble --outputDir /usr/src/app/src/assets/img/gallery/furnitures --gName=furnitures
 
 echo KONIEC
